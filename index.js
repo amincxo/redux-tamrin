ea7bcb4e1764448dce22afe9a2401c20bf96ccab
unknown <amincxo@gmail.com>
2024-10-02 02:57:05 +0330

    create store

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+//action
 function incrementCounter() {
     return {
-        type: "INCREMENT",
+        type: "INCREMENT_COUNTER",
     };
 }
+
+const initialState = {
+    counter: 0,
+    number : 5,
+};
+
+//reducer
+
+const conterreducer = (state = initialState , action) => {
+    switch(action.type){
+        case "INCREMENT_COUNTER":
+            return {
+                ...state, // immer
+                counter: counter +1 ,
+            };
+            default:
+                return state;
+    }
+};
